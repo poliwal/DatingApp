dd86116ba5fbcedd0519ca265a19294e038d53e4
poliwal <mihirpoliwal@gmail.com>
2023-02-27 16:32:24 +0530

    switched from sqlite to postgresql for pre-deployment

--- API/Program.cs
+++ API/Program.cs
@@ -19,6 +19,38 @@ builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
 
+// Uncomment this to run locally with sqlite db
+// builder.Services.AddDbContext<DataContext>(options =>
+// {
+//     options.UseSqlite("Data source=datingapp.db");
+// });
+
+// Comment when running locally
+var connString = "";
+if (builder.Environment.IsDevelopment()) 
+    connString = builder.Configuration.GetConnectionString("DefaultConnection");
+else 
+{
+// Use connection string provided at runtime by FlyIO.
+        var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+        // // Parse connection URL to connection string for Npgsql
+        connUrl = connUrl.Replace("postgres://", string.Empty);
+        var pgUserPass = connUrl.Split("@")[0];
+        var pgHostPortDb = connUrl.Split("@")[1];
+        var pgHostPort = pgHostPortDb.Split("/")[0];
+        var pgUser = pgUserPass.Split(":")[0];
+        var pgPass = pgUserPass.Split(":")[1];
+        var pgHost = pgHostPort.Split(":")[0];
+        var pgPort = pgHostPort.Split(":")[1];
+
+        connString = $"Server=dateology-db.internal;Port=5432;User Id={pgUser};Password={pgPass};Database=dateology;";
+}
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseNpgsql(connString);
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -41,9 +73,13 @@ app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.UseDefaultFiles();      //finds and serves index.html
+app.UseStaticFiles();       //finds and serves wwwroot folder
+
 app.MapControllers();
 app.MapHub<PresenceHub>("hubs/presence");
 app.MapHub<MessageHub>("hubs/message");
+app.MapFallbackToController("Index", "Fallback");
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
@@ -53,7 +89,7 @@ try
     var userManager = services.GetRequiredService<UserManager<AppUser>>();
     var roleManager = services.GetRequiredService<RoleManager<AppRole>>();
     await context.Database.MigrateAsync();
-    await context.Database.ExecuteSqlRawAsync("DELETE FROM [Connections]");
+    await Seed.ClearConnections(context);
     await Seed.SeedUsers(userManager, roleManager);
 }
 catch (Exception ex)
